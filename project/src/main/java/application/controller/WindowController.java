89e1cbfb2f09a224dbe9b797be46d533ad49a21d
feldtsen <feldtsen@gmail.com>
2020-09-15 15:43:44 +0200

    made dummy code for publish to see if I could get scrolling to work

--- project/src/main/java/application/controller/WindowController.java
+++ project/src/main/java/application/controller/WindowController.java
@@ -1,21 +1,29 @@
 package application.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 
 public class WindowController {
 
-    @FXML
-    private Canvas canvas;
 
     @FXML
-    private void drawRectangle() throws IOException {
-        //App.setRoot("secondary");
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        gc.setFill(Color.BLUE);
-        gc.fillRect(100, 100, 100, 100);
+    VBox content;
+
+    @FXML
+    private void handlePublishButton() throws IOException {
+        System.out.println("Published pressed");
+
+        content.getChildren().add(new Button("AAAA"));
+
+
+
     }
 }
