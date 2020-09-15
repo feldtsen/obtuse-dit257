09aafde7e192ac38e0791c30a2c8e583f8e38132
feldtsen <feldtsen@gmail.com>
2020-09-15 21:40:46 +0200

    demo for publishing to the board added to get the overall feel of the app

--- project/src/main/java/application/controller/WindowController.java
+++ project/src/main/java/application/controller/WindowController.java
@@ -3,10 +3,16 @@ package application.controller;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import application.App;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
@@ -18,12 +24,19 @@ public class WindowController {
     VBox content;
 
     @FXML
+    ScrollPane scrollPane;
+
+
+    private void generatePost() throws IOException {
+
+    }
+
+    @FXML
     private void handlePublishButton() throws IOException {
-        System.out.println("Published pressed");
 
-        content.getChildren().add(new Button("AAAA"));
+        HBox publishSite = FXMLLoader.load(App.class.getResource("publishSite.fxml"));
 
-
+        content.getChildren().add(publishSite);
 
     }
 }
