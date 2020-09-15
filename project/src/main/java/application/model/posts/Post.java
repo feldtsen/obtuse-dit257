3d6184a6852da2ef4563372632a4276cadf79799
Mattoom <kall0795@gmail.com>
2020-09-15 14:25:56 +0200

    Implemented abstract class Post

---
+++ project/src/main/java/application/model/posts/Post.java
@@ -0,0 +1,57 @@
+package application.model.posts;
+
+import application.model.users.IUser;
+
+public abstract class Post implements IPost {
+
+    private final String title;
+    private final IUser user;
+    private final IItem item;
+    //private final Range Availabletime;
+    private boolean claimed;
+    private boolean received;
+
+    public Post(String title, IUser user, IItem item) {
+        this.title = title;
+        this.user = user;
+        this.item = item;
+        this.claimed = false;
+        this.received = false;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public IItem getItem() {
+        return item;
+    }
+
+    @Override
+    public IUser getUser() {
+        return user;
+    }
+/*
+    @Override
+    public Range getAvailableTime() {
+        return null;
+    }
+*/
+    @Override
+    public boolean isClaimed() {
+        return claimed;
+    }
+
+    @Override
+    public boolean isReceived() {
+        return received;
+    }
+
+    @Override
+    public boolean claim(IUser user) {
+        // TODO
+        return false;
+    }
+}
