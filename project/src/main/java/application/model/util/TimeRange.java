5351f9d293903c9fbd77bde5242e59554550497c
ludvig <gussterlu@student.gu.se>
2020-09-22 14:23:30 +0200

    added range class

---
+++ project/src/main/java/application/model/util/TimeRange.java
@@ -0,0 +1,15 @@
+package application.model.util;
+
+public class TimeRange {
+
+    // Java date class
+    // Start date + time
+    // End date + time
+
+    public TimeRange(){
+
+    }
+    
+    //public getStartRange(){ return start }
+    //public getEndRange() { return end }
+}
