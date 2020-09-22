b3700c0918940dee947585c4ed6c0e290973d462
Mattoom <kall0795@gmail.com>
2020-09-22 14:07:23 +0200

    dummy client

--- project/src/main/java/application/model/client/Client.java
+++ project/src/main/java/application/model/client/Client.java
@@ -1,9 +1,11 @@
 package application.model.client;
 
+import application.model.board.Board;
 import application.model.board.IBoard;
 import application.model.users.IUser;
+import application.model.users.User;
 
-public class Client {
+public class Client implements IClient{
     private final IUser user;
     private final IBoard board;
 
@@ -18,6 +20,15 @@ public class Client {
 
     public IBoard getBoard() {
         return board;
+    }
+
+    public static IClient getTest(){
+        IUser u1 = new User("Oom", "012323422");
+        IBoard b1 = new Board();
+
+        IClient c1 = new Client(u1, b1);
+
+        return c1;
     }
 
     //boolean notifyRoom(IPost post);
