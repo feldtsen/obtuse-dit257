6ebdefc8891d68fff20bd3c134bbb22aeeee9c33
ludvig <gussterlu@student.gu.se>
2020-10-05 11:32:19 +0200

    even more parsing

--- project/src/main/java/application/model/util/TagParser.java
+++ project/src/main/java/application/model/util/TagParser.java
@@ -75,8 +75,56 @@ public class TagParser {
                     }
                 }
             }
-            for (String kategori : result){
-                System.out.println(kategori);
+            for (String tags : result){
+                System.out.println(tags);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try {
+                fileReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    public String[] getTagsForCategory (String kategori) {
+        String[] result = new String[10];
+        try
+        {
+            String line = "";
+            //Create the file reader
+            fileReader = new BufferedReader(new FileReader(fileToParse));
+            int tagkatnr = 0;
+            //Read the file line by line
+            while ((line = fileReader.readLine()) != null)
+            {
+                //Get all tokens available in line
+                String[] tokens = line.split(DELIMITER);
+                for(int i = 0; i < tokens.length; i++)
+                {
+                    if(tokens[i].equals(kategori)){
+                        result[tagkatnr] = tokens[i+1];
+                        tagkatnr++;
+                        int k = i+2;
+                        while (!(tokens[k].equals("kategori"))){
+                            result[tagkatnr] = tokens[k];
+                            tagkatnr++;
+                            if (k < tokens.length-1) {
+                                k++;
+                            } else {
+                                break;
+                            }                        }
+                    }
+                }
+            }
+            for (String category : result){
+                System.out.println(category);
             }
         }
         catch (Exception e) {
