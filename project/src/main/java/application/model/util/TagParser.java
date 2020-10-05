ac2d25415f548db5cdf0ae416e4d46696a255a6d
ludvig <gussterlu@student.gu.se>
2020-10-05 11:02:29 +0200

    added parser util class

---
+++ project/src/main/java/application/model/util/TagParser.java
@@ -0,0 +1,19 @@
+package application.model.util;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TagParser {
+    public TagParser(){
+
+    }
+
+    public String[] getAllCategories(){
+        String[] result = new String[10];
+        return result;
+    }
+    public String[] getAllTags(){
+        String[] result = new String[10];
+        return result;
+    }
+}
