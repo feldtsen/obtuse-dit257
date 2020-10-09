c8b43ce2b16d986c4fbcb6f55b4c3d98bab76611
palmdrop <anton@exlex.se>
2020-10-09 13:49:22 +0200

    moved tag displayer to own class

--- project/src/main/java/application/view/pages/publish/PublishPage.java
+++ project/src/main/java/application/view/pages/publish/PublishPage.java
@@ -19,7 +19,7 @@ public class PublishPage extends VBox implements Page {
     private final TextField titleInput;
     private final TextArea descriptionInput;
     private String type = "Donation";
-    private TagChoiceDropdown tagChoice= new TagChoiceDropdown();
+    private final TagChoiceDropdown tagChoice= new TagChoiceDropdown();
 
     private PublishPage() {
         this.setId("publishPage");
---
+++ project/src/main/java/application/view/pages/util/TagDisplay.java
@@ -0,0 +1,23 @@
+package application.view.pages.util;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+import java.util.Set;
+
+public class TagDisplay extends HBox {
+
+    public TagDisplay(Set<String> tags) {
+        // Create and initialize container for tags
+        this.getChildren().add(new Label("Tags: ")); // Initial text
+        int i = 0; // Counter used to determine when last tag is reached
+
+        for(String tag : tags) {
+            // Add delimiter, comma between tags and nothing when the last tag is reached
+            String delimiter = i != tags.size() - 1 ? ", " : "";
+            this.getChildren().add(new Label(tag + delimiter));
+            i++;
+        }
+    }
+
+}
