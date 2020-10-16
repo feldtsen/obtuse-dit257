1a6c572744d85b7900fa5f83466b5596b8f357c3
feldtsen <feldtsen@gmail.com>
2020-10-16 19:21:22 +0200

    added tooltip to navigation buttons

--- project/src/main/java/application/view/navigation/BoardNavigationButton.java
+++ project/src/main/java/application/view/navigation/BoardNavigationButton.java
@@ -5,6 +5,7 @@ import application.controller.BoardController;
 import application.view.pages.PageParent;
 import application.view.pages.board.BoardPage;
 import application.view.util.SVGHelper;
+import javafx.scene.control.Tooltip;
 
 public class BoardNavigationButton extends NavigationButton {
     private static BoardNavigationButton instance = null;
@@ -12,8 +13,16 @@ public class BoardNavigationButton extends NavigationButton {
     private BoardNavigationButton () {
         super();
 
-        this.setGraphic(SVGHelper.createIcon("M296 32h192c13.255 0 24 10.745 24 24v160c0 13.255-10.745 24-24 24H296c-13.255 0-24-10.745-24-24V56c0-13.255 10.745-24 24-24zm-80 0H24C10.745 32 0 42.745 0 56v160c0 13.255 10.745 24 24 24h192c13.255 0 24-10.745 24-24V56c0-13.255-10.745-24-24-24zM0 296v160c0 13.255 10.745 24 24 24h192c13.255 0 24-10.745 24-24V296c0-13.255-10.745-24-24-24H24c-13.255 0-24 10.745-24 24zm296 184h192c13.255 0 24-10.745 24-24V296c0-13.255-10.745-24-24-24H296c-13.255 0-24 10.745-24 24v160c0 13.255 10.745 24 24 24z", 0.05));
+        this.setGraphic(SVGHelper.createIcon("M296 32h192c13.255 0 24 10.745 24 24v160c0 13.255-10.745 24-24 " +
+                "24H296c-13.255 0-24-10.745-24-24V56c0-13.255 10.745-24 24-24zm-80 0H24C10.745 32 0 42.745 0 56v160c0 " +
+                "13.255 10.745 24 24 24h192c13.255 0 24-10.745 24-24V56c0-13.255-10.745-24-24-24zM0 296v160c0 13.255 " +
+                "10.745 24 24 24h192c13.255 0 24-10.745 24-24V296c0-13.255-10.745-24-24-24H24c-13.255 0-24 10.745-24 " +
+                "24zm296 184h192c13.255 0 24-10.745 24-24V296c0-13.255-10.745-24-24-24H296c-13.255 0-24 10.745-24 " +
+                "24v160c0 13.255 10.745 24 24 24z", 0.05));
+
         this.setOnMouseClicked(e->this.action());
+
+        this.setTooltip(new Tooltip("Takes you to all the published posts"));
 
     }
 
