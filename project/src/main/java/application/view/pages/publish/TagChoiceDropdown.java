a43d494b2606995a22c96bfe23a6c665830bfe66
Mattoom <kall0795@gmail.com>
2020-10-16 11:54:25 +0200

    can now remove tags from publish post
    Co-authored-by: palmdrop <anton@exlex.se>

--- project/src/main/java/application/view/pages/publish/TagChoiceDropdown.java
+++ project/src/main/java/application/view/pages/publish/TagChoiceDropdown.java
@@ -1,37 +1,72 @@
 package application.view.pages.publish;
 
 import application.model.client.Client;
-import application.model.client.TagParser;
 import application.view.pages.util.TagDisplay;
 import javafx.collections.FXCollections;
 import javafx.event.Event;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class TagChoiceDropdown extends VBox {
-    private final static TagParser tagParser = Client.getInstance().getTagParser();
-    private final Set<String> tags = new HashSet<>();
+    private final Set<String> chosenTags = new HashSet<>();
     private final ComboBox<String> tagChoices;
-    private final TagDisplay tagDisplay;
+    private final FlowPane tagDisplay;
 
     public TagChoiceDropdown(){
-        tagChoices = new ComboBox<>(FXCollections.observableArrayList(tagParser.getAllTags()));
+        tagChoices = new ComboBox<>(FXCollections.observableArrayList(Client.getInstance().getTagParser().getAllTags()));
         tagChoices.getStyleClass().add("tagDropdown");
         tagChoices.setOnHidden(this::action);
-        tagDisplay = new TagDisplay(tags);
+
+        this.tagDisplay = new FlowPane();
         this.getChildren().addAll(tagChoices, tagDisplay);
     }
-    private void action(Event e){
-        if(!tagChoices.getSelectionModel().isEmpty()) {
-            tags.add(tagChoices.getSelectionModel().getSelectedItem());
-            tagChoices.getSelectionModel().clearSelection();
-            tagDisplay.setTags(tags);
+
+    private void setTags(Set<String> tags) {
+        tagDisplay.getChildren().clear();
+        tagDisplay.getStyleClass().add("tagContainer");
+
+        tagDisplay.setVgap(10);
+        tagDisplay.setHgap(10);
+
+        for(String tag : tags) {
+            HBox tagBox = new HBox();
+            Label tagLabel = new Label(tag);
+
+            Button deleteButton = new Button();
+            deleteButton.setText("X");
+            //TODO: styling
+
+            tagBox.getChildren().addAll(tagLabel, deleteButton);
+            tagBox.getStyleClass().add("tag");
+
+            tagDisplay.getChildren().add(tagBox);
+
+            // Delete tag on button press
+            deleteButton.setOnAction(e -> {
+                tagDisplay.getChildren().remove(tagBox);
+                chosenTags.remove(tag);
+            });
+
         }
     }
-    public Set<String> getTags(){
-        return tags;
+
+    private void action(Event e){
+        if(!tagChoices.getSelectionModel().isEmpty()) {
+            String tag = tagChoices.getSelectionModel().getSelectedItem();
+            chosenTags.add(tag);
+            tagChoices.getSelectionModel().clearSelection();
+            setTags(chosenTags);
+        }
+    }
+
+    public Set<String> getChosenTags(){
+        return chosenTags;
     }
 }
