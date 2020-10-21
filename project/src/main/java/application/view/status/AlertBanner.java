187eacb24c81c16d6a42cfc29620217359bbbbfb
Mattoom <kall0795@gmail.com>
2020-10-21 12:17:04 +0200

    added comments in AlertBanner
    Co-authored-by: feldtsen <feldtsen@gmail.com>

--- project/src/main/java/application/view/status/AlertBanner.java
+++ project/src/main/java/application/view/status/AlertBanner.java
@@ -5,6 +5,7 @@ import javafx.scene.Group;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 
+// Popup alert displayed to the user
 public class AlertBanner extends Group {
     private static AlertBanner instance = null;
     // How long the alert message will be visible
@@ -14,7 +15,6 @@ public class AlertBanner extends Group {
         // If this is set on all components in a HBox, it will split the space evenly
 
         this.getStyleClass().add("alertBanner");
-
     }
 
     // Singleton
@@ -24,19 +24,22 @@ public class AlertBanner extends Group {
         return instance;
     }
 
+    // Decides what to display
     public void setAlertMessage(String alertMessage, Alert.AlertType alertType) {
         Label label = new Label();
         label.setText("    " + alertMessage + "     ");
 
+        // To make sure the message gets the correct color associated with what type of alert it is, we clear the style
+        // then reapply
         label.getStyleClass().clear();
         label.getStyleClass().add("alertBanner");
 
+        // The container for the message gets added
         this.getChildren().setAll(label);
+
         // We gibe the message different colors based on what type of alert it is
         if (alertType == Alert.AlertType.ERROR)            label.getStyleClass().add("error");
         else if (alertType == Alert.AlertType.CONFIRMATION)label.getStyleClass().add("confirmation");
-
-
 
         // We start a new thread so that the app doesn't freeze while a message is being displayed
         new Thread(() -> {
