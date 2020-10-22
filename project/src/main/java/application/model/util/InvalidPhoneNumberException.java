5fbade76b6a94ae0db45eebcc93e3b74ec7b28b9
palmdrop <anton@exlex.se>
2020-10-22 11:52:44 +0200

    more comments

--- project/src/main/java/application/model/util/InvalidPhoneNumberException.java
+++ project/src/main/java/application/model/util/InvalidPhoneNumberException.java
@@ -1,5 +1,6 @@
 package application.model.util;
 
+// Exception which is thrown when an invalid phone number is entered
 public class InvalidPhoneNumberException extends Exception {
     public InvalidPhoneNumberException(String message) {
         super(message);
--- project/src/main/java/application/model/util/PhoneNumber.java
+++ project/src/main/java/application/model/util/PhoneNumber.java
@@ -3,19 +3,24 @@ package application.model.util;
 import java.io.Serializable;
 import java.util.regex.Pattern;
 
+// Class representing phone numbers of users
 public class PhoneNumber implements Serializable {
+    // Regex for matching valid phone numbers
     private final static String VALID_PATTERNS
             = "^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$"
             + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?){2}\\d{3}$"
             + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?)(\\d{2}[ ]?){2}\\d{2}$";
 
+    // The actual phone number
     private final String number;
 
     public PhoneNumber(String number) throws InvalidPhoneNumberException {
+        // Throw exception if phone number is invalid
         if(!isValidNumber(number)) throw new InvalidPhoneNumberException(number + " is not a valid phone number.");
         this.number = number;
     }
 
+    // Validate phone number
     private boolean isValidNumber(String number) {
         Pattern pattern = Pattern.compile(VALID_PATTERNS);
         return pattern.matcher(number).matches();
